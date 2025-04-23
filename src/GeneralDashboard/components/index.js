4f87834456ab8ad410047fefe32b7b8b916f7730
keshav-gupta01 <keshavgupta.contact@gmail.com>
2025-04-23 09:23:12 +0530

    test

--- src/GeneralDashboard/components/index.js
+++ src/GeneralDashboard/components/index.js
@@ -1,12 +1,8 @@
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import Companies from "./Companies";
-import About from "./About";
-import Courses from "./Courses";
-import Achievement from "./Achievement";
-import Categories from "./Categories";
 import FeedBack from "./FeedBack";
 import CTA from "./CTA";
 import Footer from "./Footer";
 
-export { Navbar,HeroSection,Companies,About,FeedBack,CTA,Footer }
+export { Navbar,HeroSection,Companies,FeedBack,CTA,Footer }
